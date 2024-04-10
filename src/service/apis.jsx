0751f2c60221a157d4e1422182503d2e9ef85c63
Apoorv jain <apoorvjain7222@gmail.com>
2024-04-10 07:25:01 +0530

    apis

--- src/service/apis.jsx
+++ src/service/apis.jsx
@@ -1,9 +1,14 @@
 const api = 'http://195.35.20.207:3000'
 
 export default {
-    REGISTERAPI:`${api}/auth/doctor/save`,
-    LOGINAPI:`${api}/auth/login`,
-    RECEPTIONAPI:`${api}/api/reception/save`,
-    RECEPTIONLISTAPI:`${api}/api/reception/lists`,
-    NEWPATIENTAPI:`${api}/api/patient/addpatient`
+    REGISTERAPI:`${api}/auth/doctor/save`,                                               // POST
+    LOGINAPI:`${api}/auth/login`,                                                        // POST
+    RECEPTIONLOGINAPI:`${api}/auth/login`,                                               // POST
+    RECEPTIONAPI:`${api}/api/reception/save`,                                            // POST
+    RECEPTIONLISTAPI:`${api}/api/reception/lists`,                                       // GET
+    NEWPATIENTAPI:`${api}/api/patient/addpatient`,                                       // POST
+    RECEPTIONDETAILUPDATEAPI:`${api}/api/reception/updateReception/:id`,                 // PUT
+    RECEPTIONDELETEAPI:`${API}/api/reception/delete/3`,                                  // PUT
+    PATIENTLISTFORDOCTORAPI:`${api}/api/patient/list`,                                   // GET
+    PATIENTDETAILUPDATE:`${api}/api/patient/update/6`
 }
