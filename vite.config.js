0d1fe95f8f326cf7514361a13766342d1acaff51
Apoorv jain <apoorvjain7222@gmail.com>
2024-07-08 23:25:19 +0530

    last

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This will bind to all network interfaces
+    port: 5173 // You can choose any port, but make sure it matches your Render configuration
+  }
 })
